b8d23f8b1daebf5c87089a73557c47376669d061
Mr. Sableye <mister.sableye@gmail.com>
2021-01-17 23:51:50 -0800

    Updates doubles banlist

--- config/formats.ts
+++ config/formats.ts
@@ -123,7 +123,7 @@ export const Formats: FormatList = [
 			'Fochun Only',
 			'Blobbos Clause',
 		],
-		banlist: ['Uber', 'Arena Trap', 'Moody', 'Power Construct', 'Shadow Tag', 'Baton Pass', 'Wonder Guard'],
+		banlist: ['Blobbosinite', 'Uber', 'Arena Trap', 'Moody', 'Power Construct', 'Shadow Tag', 'Baton Pass', 'Wonder Guard'],
 	},
 	{
 		section: "Clover x Pokemon Singles"
