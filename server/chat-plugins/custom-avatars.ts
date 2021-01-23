c09851c771b95fbc015ab41d58ae779e3afa146f
Mr. Sableye <mister.sableye@gmail.com>
2021-01-22 22:56:36 -0800

    Adds Blobbos command for Blobbos avatar

---
+++ server/chat-plugins/custom-avatars.ts
@@ -0,0 +1,10 @@
+export const commands: ChatCommands = {
+	blobbos(target, room, user) {
+		if (Config.blobbosTournamentWinners && Config.blobbosTournamentWinners.includes(user.id)) {
+			this.user.avatar = '#blobbos';
+
+			const avatarUrl = 'trainers-custom/blobbos.png';
+			this.sendReply(`${this.tr`Avatar changed to:`}\n|raw|<img src="//${Config.routes.client}/sprites/${avatarUrl}" alt="${'#blobbos'}" width="80" height="80" class="pixelated" />`);
+		}
+	},
+};
