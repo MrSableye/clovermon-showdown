b416513a5d82088d8dd8636adc29d0fc65c3c56c
Mr. Sableye <mister.sableye@gmail.com>
2021-02-06 19:17:54 -0800

    Fixes item validation for Clover Only ruleset

--- data/mods/clover/rulesets.ts
+++ data/mods/clover/rulesets.ts
@@ -12,25 +12,25 @@ export const Formats: {[k: string]: ModdedFormatData} = {
 	cloveronly: {
 		effectType: 'ValidatorRule',
 		name: 'Clover Only',
-		desc: "Only allows Pok&eacute;mon, items, and moves available in Pok&eacute;mon Clover.",
+		desc: "Only allows Pokémon, items, and moves available in Pokémon Clover.",
 		onValidateSet(set) {
 			const errors = [];
 
 			const species = this.dex.getSpecies(set.species || set.name);
 			if (!species.availability?.clover) {
-				errors.push(`${species.baseSpecies} is not in Pok&eacute;mon Clover.`);
+				errors.push(`${species.baseSpecies} is not in Pokémon Clover.`);
 			}
 
 			const item = this.dex.getItem(set.item);
-			if (!item.availability?.clover) {
-				errors.push(`${set.name || set.species} has ${item.name}, which is unavailable in Pok&eacute;mon Clover.`);
+			if (item && item.id && item.id !== '' && !item.availability?.clover) {
+				errors.push(`${set.name || set.species} has ${item.name}, which is unavailable in Pokémon Clover.`);
 			}
 
 			set.moves.forEach((moveName) => {
 				const move = this.dex.getMove(this.toID(moveName));
 
 				if (!move.availability?.clover) {
-					errors.push(`${set.name || set.species} has ${move}, which is unavailable in Pok&eacute;mon Clover.`);
+					errors.push(`${set.name || set.species} has ${move}, which is unavailable in Pokémon Clover.`);
 				}
 			});
 
