7376dceb8933868393a360693613109b207b8da7
Mr. Sableye <mister.sableye@gmail.com>
2021-02-14 22:31:39 -0800

    Fixes availability validation for Atlas

--- data/mods/atlas/rulesets.ts
+++ data/mods/atlas/rulesets.ts
@@ -7,20 +7,20 @@ export const Formats: {[k: string]: ModdedFormatData} = {
 			const errors = [];
 
 			const species = this.dex.getSpecies(set.species || set.name);
-			if (!species.availability?.atlas) {
+			if (species.availability?.atlas !== 1) {
 				errors.push(`${species.baseSpecies} is not in Pokémon Atlas.`);
 			}
 
 			const item = this.dex.getItem(set.item);
 			// TODO: Remove Clover check
-			if (item && item.id && item.id !== '' && (!item.availability?.atlas && !item.availability?.clover)) {
+			if (item && item.id && item.id !== '' && (item.availability?.atlas !== 1 && item.availability?.clover !== 1)) {
 				errors.push(`${set.name || set.species} has ${item.name}, which is unavailable in Pokémon Atlas.`);
 			}
 
 			set.moves.forEach((moveName) => {
 				const move = this.dex.getMove(this.toID(moveName));
 				// TODO: Remove Clover check
-				if (!this.toID(moveName).startsWith('hiddenpower') && (!item.availability?.atlas && !item.availability?.clover)) {
+				if (!this.toID(moveName).startsWith('hiddenpower') && (item.availability?.atlas !== 1 && item.availability?.clover !== 1)) {
 					errors.push(`${set.name || set.species} has ${move}, which is unavailable in Pokémon Atlas.`);
 				}
 			});
