cd5cf2ddab53d507049af114e98df81f7cb08894
The Clown <84112692+TheClown12@users.noreply.github.com>
2021-05-17 14:55:24 -0400

    Fix spelling in belzeburro and karabojek. Enabled 3.5 mons that are fully coded (#29)
    * Fixed minor errors
    
    Fix spelling in belzeburro and karabojek. Enabled 3.5 mons that are fully coded
    
    * added and enabled tauran line

--- data/mods/atlas/formats-data.ts
+++ data/mods/atlas/formats-data.ts
@@ -739,9 +739,60 @@ export const FormatsData: { [k: string]: ModdedSpeciesFormatsData } = {
 		tier: "NFE",
 		isNonstandard: null,
 	},
-	bezelburro: {
+	belzeburro: {
+		inherit: true,
+		tier: "OU",
+		isNonstandard: null,
+	},
+	spikmon: {
+		inherit: true,
+		tier: "LC",
+		isNonstandard: null,
+	},
+	lavostra: {
+		inherit: true,
+		tier: "OU",
+		isNonstandard: null,
+	},
+	yoshole: {
+		inherit: true,
+		tier: "LC",
+		isNonstandard: null,
+	},
+	dracrime: {
+		inherit: true,
+		tier: "OU",
+		isNonstandard: null,
+	},
+	gieugon: {
+		inherit: true,
+		tier: "LC",
+		isNonstandard: null,
+	},
+	giyagon: {
+		inherit: true,
+		tier: "OU",
+		isNonstandard: null,
+	},
+	gnelf: {
+		inherit: true,
+		tier: "LC",
+		isNonstandard: null,
+	},
+	gnolem: {
+		inherit: true,
+		tier: "OU",
+		isNonstandard: null,
+	},
+	karaojek: {
+		inherit: true,
+		tier: "LC",
+		isNonstandard: null,
+	},
+	tauran: {
 		inherit: true,
 		tier: "OU",
 		isNonstandard: null,
 	},
 };
+
