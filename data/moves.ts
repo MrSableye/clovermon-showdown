f019044397a5708abcd4224ae95e0f30c94ba6a8
Mr. Sableye <mister.sableye@gmail.com>
2021-08-29 01:01:57 -0500

    Fixes details text

--- data/moves.ts
+++ data/moves.ts
@@ -22862,9 +22862,7 @@ export const Moves: { [moveid: string]: MoveData } = {
 					const level = (targetSet.level && targetSet.level !== 100) ? `, L${targetSet.level}` : '';
 					const gender = (targetSet.gender === 'M' || targetSet.gender === 'F') ? `, ${targetSet.gender}` : '';
 					const shiny = targetSet.shiny ? `, shiny` : '';
-					const details = `${targetSet.species}, ${level}${gender}${shiny}, spawned`;
-					this.debug(`child details: ${details}, species: ${targetSet.species}, level: ${level}, gender: ${gender}, shiny: ${shiny}`);
-					this.add('poke', sourceSide.id, `${targetSet.species}, ${level}${gender}${shiny}, spawned`, '');
+					this.add('poke', sourceSide.id, `${targetSet.species}${level}${gender}${shiny}, spawned`, '');
 					source.speciesState['parent'] = true;
 				} else {
 					this.add('-fail', source, 'move: Mating Press');
