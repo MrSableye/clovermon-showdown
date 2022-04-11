296f4cda426c9b2f57df902045fbd820db59b3e7
Mr. Sableye <mister.sableye@gmail.com>
2022-04-10 19:16:13 -0500

    Fixes Protean

--- data/mods/clover/abilities.ts
+++ data/mods/clover/abilities.ts
@@ -26,6 +26,17 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 			}
 		},
 	},
+	protean: {
+		inherit: true,
+		onPrepareHit(source, target, move) {
+			if (move.hasBounced || move.sourceEffect === 'snatch') return;
+			const type = move.type;
+			if (type && source.getTypes().join() !== type) {
+				if (!source.setType(type)) return;
+				this.add('-start', source, 'typechange', type, '[from] ability: Protean');
+			}
+		},
+	},
 	/* Clover CAP Abilities */
 	cakeveil: {
 		inherit: true,
