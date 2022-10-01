1c9e68d6af52429c22abb71b30e4f6aea7a87bcc
Mr. Sableye <mister.sableye@gmail.com>
2022-10-01 18:42:35 -0500

    Updates flairs to be able to be moved up and down slightly

--- server/chat-plugins/custom-flair.ts
+++ server/chat-plugins/custom-flair.ts
@@ -3,6 +3,9 @@ import {Badges} from './badges';
 
 const ERROR_USER_INELIGIBLE = 'You are not eligble for a custom flair.';
 
+const defaultHeight = -6;
+const maxHeightChange = 10;
+
 const baseUrls: Record<string, string> = {
 	pokemon: 'https://raw.githubusercontent.com/Jackinev/pokeicons/master/40x30',
 	clover: 'https://raw.githubusercontent.com/MrSableye/clovermon-showdown-assets/master/clover/sprites/pokemon-icons',
@@ -16,13 +19,13 @@ const getBaseUrl = (pokemonMod: string) => baseUrls[pokemonMod] || baseUrls.clov
 
 const getFlairUrl = (pokemonId: string, pokemonMod: string) => `${getBaseUrl(pokemonMod)}/${pokemonId}.png`;
 
-const createUserCss = (userId: string, pokemonId: string, pokemonMod: string) => `[id$="-userlist-user-${userId}"]{background: url("${getFlairUrl(pokemonId, pokemonMod)}") no-repeat right -7px center;}`;
+const createUserCss = (userId: string, pokemonId: string, pokemonMod: string, heightOffset: number) => `[id$="-userlist-user-${userId}"]{background: url("${getFlairUrl(pokemonId, pokemonMod)}") no-repeat right -7px top ${heightOffset}px;}`;
 
-const createCss = (cssConfig: CssConfig): string => [CSS_HEADER, ...Object.entries(cssConfig).map(([userId, {pokemonId, pokemonMod}]) => createUserCss(userId, pokemonId, pokemonMod))].join('\n');
+const createCss = (cssConfig: CssConfig): string => [CSS_HEADER, ...Object.entries(cssConfig).map(([userId, {pokemonId, pokemonMod, heightOffset}]) => createUserCss(userId, pokemonId, pokemonMod, heightOffset))].join('\n');
 
 const writeCss = (content: string) => FS('config/custom.css').writeSync(content);
 
-type CssConfig = Record<string, { pokemonId: string, pokemonMod: string }>;
+type CssConfig = Record<string, { pokemonId: string, pokemonMod: string, heightOffset: number }>;
 
 const cssConfig: CssConfig = JSON.parse(
 	FS('config/chat-plugins/custom-flair.json').readIfExistsSync() || "{}"
@@ -58,8 +61,18 @@ export const commands: Chat.ChatCommands = {
 				throw new Chat.ErrorMessage(ERROR_USER_INELIGIBLE);
 			}
 
-			const [pokemonId, pokemonMod] = target.split(',').map(toID);
-			const flairUrl = getFlairUrl(pokemonId, pokemonMod);
+			const [pokemon, pokemonMod, heightInput] = target.split(',');
+			const flairUrl = getFlairUrl(toID(pokemon), toID(pokemonMod));
+
+			let heightOffset = parseInt(heightInput);
+
+			if (Number.isNaN(heightOffset)) {
+				heightOffset = defaultHeight;
+			} else if (Math.abs(heightOffset) > maxHeightChange) {
+				heightOffset = defaultHeight;
+			} else {
+				heightOffset = defaultHeight + Math.trunc(heightOffset);
+			}
 
 			try {
 				await Net(flairUrl).get();
@@ -67,10 +80,15 @@ export const commands: Chat.ChatCommands = {
 				return this.errorReply('Invalid Clovermon name.');
 			}
 
-			cssConfig[user.id] = {pokemonId, pokemonMod};
+			cssConfig[user.id] = {pokemonId: toID(pokemon), pokemonMod: toID(pokemonMod), heightOffset};
 			saveCssConfig();
 
 			this.sendReply("|raw| Your flair was successfully set. It may take a while for it to show up. Flair:<br /><img src='" + flairUrl + "' width='40' height='30'>");
 		},
+	},
+	flairhelp() {
+		this.sendReplyBox(
+			`<code>/flair set [pokemon, mod, heightOffset]</code>: sets your flair to the desired pokemon from the specified mod (pokemon or clover) with an optional height adjustment.<br />`,
+		);
 	},
 };
