9cdd059a385e7c9a0dd8bc21f714fba1728e100a
Mr. Sableye <mister.sableye@gmail.com>
2022-10-17 15:45:48 -0500

    Fixes Tombstoner

--- data/moves.ts
+++ data/moves.ts
@@ -21321,8 +21321,8 @@ export const Moves: {[moveid: string]: MoveData} = {
 		pp: 5,
 		priority: 0,
 		flags: {protect: 1, mirror: 1},
-		basePowerCallback(pokemon, target, move) {
-			return move.basePower * pokemon.hp / pokemon.maxhp;
+		onModifyMove(move, pokemon) {
+			move.basePower = move.basePower * pokemon.hp / pokemon.maxhp;
 		},
 		selfdestruct: "always",
 		overrideOffensiveStat: "def",
