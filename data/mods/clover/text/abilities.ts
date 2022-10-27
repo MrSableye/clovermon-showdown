19794bdfb1f54828fb4c92ebb93f792237308462
Mr. Sableye <mister.sableye@gmail.com>
2022-10-27 11:52:38 -0500

    Modifies text for Clover ability changes to use new modded text system

---
+++ data/mods/clover/text/abilities.ts
@@ -0,0 +1,12 @@
+export const AbilitiesText: {[k: string]: ModdedAbilityText} = {
+	galewings: {
+		inherit: true,
+		shortDesc: "This Pokemon's Flying-type moves have their priority increased by 1.",
+		desc: "This Pokemon's Flying-type moves have their priority increased by 1.",
+	},
+	illuminate: {
+		inherit: true,
+		desc: "This Pokemon's moves have their accuracy multiplied by 1.3.",
+		shortDesc: "This Pokemon's moves have their accuracy multiplied by 1.3.",
+	},
+};
