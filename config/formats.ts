bf15487aacab07e9a5183fc38093ebf07461a1cf
Greednut <115999607+Greednut@users.noreply.github.com>
2022-12-26 17:43:49 -0300

    lol i forgot these
    my bad

--- config/formats.ts
+++ config/formats.ts
@@ -795,7 +795,7 @@ export const Formats: FormatList = [
 			'OHKO Clause',
 			'Evasion Moves Clause',
 		],
-		banlist: ['Baton Pass', 'Moody', 'Arena Trap', 'Shadow Tag','Autumn','Doomsday','Doomsday-Revenant','Fusjite','Eternatus-Eternamax'],
+		banlist: ['Baton Pass', 'Moody', 'Arena Trap', 'Shadow Tag','Autumn','Doomsday','Doomsday-Revenant','Fusjite','Eternatus-Eternamax','Fuck You','Eternal Walk','Cope','Francine',"It's Over"],
 		
 	},
 	{
