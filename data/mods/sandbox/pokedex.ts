2ba991d92919170d674486c5ef61c5ab7c798231
Mr. Sableye <mister.sableye@gmail.com>
2023-01-05 18:59:45 -0600

    Buffs Adventurer even more but only in sandbox

---
+++ data/mods/sandbox/pokedex.ts
@@ -0,0 +1,24 @@
+export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+	blobbosadventurer: {
+		inherit: true,
+		baseStats: {
+			hp: 85,
+			atk: 85,
+			def: 85,
+			spa: 85,
+			spd: 85,
+			spe: 85,
+		},
+	},
+	blobbosadventurerlegendary: {
+		inherit: true,
+		baseStats: {
+			hp: 110,
+			atk: 130,
+			def: 110,
+			spa: 130,
+			spd: 110,
+			spe: 130,
+		},
+	},
+};
