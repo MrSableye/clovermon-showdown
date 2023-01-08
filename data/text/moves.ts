bd84ca155f33f0a809eb58c8fa601bf6b2ec7eb6
Karma <115958638+KarmaAttackCouncil@users.noreply.github.com>
2023-01-07 22:59:04 -0500

    slip and slide

--- data/text/moves.ts
+++ data/text/moves.ts
@@ -7534,8 +7534,8 @@ export const MovesText: {[k: string]: MoveText} = {
 	},
 	faeblade: {
 		name: "Faeblade",
-		desc: "This move becomes a physical attack that makes contact if the value of ((((2 * the user's level / 5 + 2) * 90 * X) / Y) / 50), where X is the user's Attack stat and Y is the target's Defense stat, is greater than the same value where X is the user's Special Attack stat and Y is the target's Special Defense stat. No stat modifiers other than stat stage changes are considered for this purpose. If the two values are equal, this move chooses a damage category at random.",
-		shortDesc: "Physical+contact if it would be stronger.",
+		desc: "Deals damage to the target based on its Special Defense instead of Defense.",
+		shortDesc: "Damages target based on Sp. Def, not Defense.",
 	},
 	stickytongue: {
 		name: "Sticky Tongue",
