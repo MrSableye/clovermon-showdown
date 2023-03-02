d3229a7593fa63150fad8057eb8138109f1a09fe
Mr. Sableye <mister.sableye@gmail.com>
2023-03-02 01:04:35 -0600

    [CAB] Updates Strife: Dicekind description

--- data/text/moves.ts
+++ data/text/moves.ts
@@ -8801,6 +8801,6 @@ export const MovesText: {[k: string]: MoveText} = {
 	},
 	strifedicekind: {
 		name: "Strife: Dicekind",
-		desc: "Hits 8 times. Each hit can miss. 4% chance to either burn, freeze, paralyze, boost a foe's random stat by 1, or lower a foe's random stat by 1.",
+		desc: "Physical if user's Atk > Sp. Atk. Hits 8 times. Each hit can miss. 4% chance to either burn, freeze, paralyze, boost a foe's random stat by 1, or lower a foe's random stat by 1.",
 	},
 };
