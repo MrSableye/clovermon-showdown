7ecc984dc1dbe5e53d781ea821934a328d1a65c4
Mr. Sableye <mister.sableye@gmail.com>
2023-03-02 18:59:12 -0600

    Fixes typo

--- data/text/abilities.ts
+++ data/text/abilities.ts
@@ -21,7 +21,7 @@ export const AbilitiesText: {[k: string]: AbilityText} = {
 		name: "Aftermath",
 		desc: "If this Pokemon is knocked out with a contact move, that move's user loses 1/4 of its maximum HP, rounded down. If any active Pokemon has the Damp Ability, this effect is prevented.",
 		shortDesc: "If this Pokemon is KOed with a contact move, that move's user loses 1/4 its max HP.",
-F
+
 		damage: "  [POKEMON] was hurt!",
 	},
 	airlock: {
