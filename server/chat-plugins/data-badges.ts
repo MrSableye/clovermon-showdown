11228bac6e9d399bd0c07ce6fe5bacffbbc3f584
Mr. Sableye <mister.sableye@gmail.com>
2023-07-05 14:01:16 -0500

    Adds ability to activate Discord badge

--- server/chat-plugins/data-badges.ts
+++ server/chat-plugins/data-badges.ts
@@ -1,7 +1,9 @@
+import {REST, Routes} from 'discord.js';
 import {FS} from '../../lib';
 import {Badges} from './badges';
 
 const DISCORD_BOT_ID = "mrsablebot"; // TODO: Make this configurable
+const DISCORD_BADGE_ID = "discord";
 const MINIMUM_TOURS_REQUIRED = 4;
 const TOUR_BADGE_ID = "tourfarmer";
 const OTHER_BADGES: [number, string][] = [
@@ -88,6 +90,18 @@ const checkTourThreshold = async (userID: string, user: User) => {
 	return false;
 };
 
+const addDiscordBadge = async (user: User, username: string) => {
+	try {
+		try {
+			await Badges.addBadgeToUser(user.id, DISCORD_BADGE_ID, user, true);
+		} catch (e) {}
+
+		await Badges.updateBadgeData(user.id, DISCORD_BADGE_ID, { username, }, user, true);
+	} catch (e) { return false; }
+
+	return true;
+};
+
 export const commands: Chat.ChatCommands = {
 	dbadge: 'databadge',
 	databadge: {
@@ -155,6 +169,22 @@ export const commands: Chat.ChatCommands = {
 			);
 		},
 		discord: {
+			async activate(target, room, user) {
+				if (!Config.discord) throw new Chat.ErrorMessage('Discord functionality is not enabled.');
+
+				const discordId = data.discord[user.id];
+
+				if (!discordId) throw new Chat.ErrorMessage('You have no linked Discord id.');
+
+				const rest = new REST().setToken(Config.discord);
+				const discordUser = await rest.get(Routes.user(discordId)) as any;
+				const username = discordUser.global_name || `${discordUser.username}#${discordUser.discriminator}`;
+
+				const success = await addDiscordBadge(user, username);
+				if (!success) throw new Chat.ErrorMessage('An error has occured.');
+				
+				return this.sendReplyBox('Your Discord badge has been activated and updated.');
+			},
 			get(target, room, user) {
 				const discordId =  data.discord[user.id];
 				if (!discordId) throw new Chat.ErrorMessage('You have no linked Discord id.');
