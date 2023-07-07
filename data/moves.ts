6b3a59174405d34ddb3354639a75794b8c2dea94
Mr. Sableye <mister.sableye@gmail.com>
2023-07-06 21:52:24 -0500

    Fixes Star Force

--- data/moves.ts
+++ data/moves.ts
@@ -31603,7 +31603,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 				}
 			}
 
-			if (this.randomChance(9 - Math.max(starforce, 8), 8)) {
+			if (this.randomChance(Math.max(1, Math.min(9 - starforce, 8)), 8)) {
 				for (let i = 0; i < starforce + 2; i++) {
 					const randomStat = this.sampleNoReplace(stats);
 					const boost: SparseBoostsTable = {};
