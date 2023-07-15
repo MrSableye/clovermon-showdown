42ecda2d9152dbcbea0eee43ff5b826d9c166437
SDM0 <60699143+SDM0@users.noreply.github.com>
2023-07-15 02:46:11 +0200

    Added Wack ruleset

---
+++ data/mods/wack/rulesets.ts
@@ -0,0 +1,8 @@
+export const Rulesets: {[k: string]: ModdedFormatData} = {
+	standard: {
+		inherit: true,
+		ruleset: [
+			'Obtainable', 'Team Preview', 'Sleep Clause Mod', 'Species Clause', 'Nickname Clause', 'OHKO Clause', 'Evasion Items Clause', 'Evasion Moves Clause', 'Endless Battle Clause', 'HP Percentage Mod', 'Cancel Mod',
+		],
+	},
+}
