4e90369fa671db8f39d046e95ca6088043260fac
Mr. Sableye <mister.sableye@gmail.com>
2023-07-20 18:13:37 -0500

    Fixes default behavior for /custom

--- server/chat-plugins/custom.ts
+++ server/chat-plugins/custom.ts
@@ -307,19 +307,19 @@ export const commands: Chat.ChatCommands = {
 					this.sendReply(`${this.tr`Avatar changed to:`}\n|raw|${createAvatarHtml('blobbos', true)}`);
 				}
 			},
-		},
-		'': 'avatarhelp',
-		avatarhelp() {
-			this.sendReplyBox(
-				`<code>/custom avatar request [image url]</code>: requests a custom avatar. Requires: custom avatar access<br />` +
-				`<code>/custom avatar showall</code>: shows all approved avatars. Requires: @ or above<br />` +
-				`<code>/custom avatar showrequests</code>: shows all un-approved avatars. Requires: @ or above<br />` +
-				`<code>/custom avatar approve [user]</code>: approves the user's avatar request. Requires: @ or above<br />` +
-				`<code>/custom avatar deny [user]</code>: denies the user's avatar request. Requires: @ or above<br />` +
-				`<code>/custom avatar on</code>: enables your own custom avatar.<br />` +
-				`<code>/custom avatar off</code>: disables your own custom avatar.<br />` +
-				`<code>/custom avatar blobbos</code>: enables the covetted Blobbos avatar.<br />`
-			);
+			'': 'help',
+			help() {
+				this.sendReplyBox(
+					`<code>/custom avatar request [image url]</code>: requests a custom avatar. Requires: custom avatar access<br />` +
+					`<code>/custom avatar showall</code>: shows all approved avatars. Requires: @ or above<br />` +
+					`<code>/custom avatar showrequests</code>: shows all un-approved avatars. Requires: @ or above<br />` +
+					`<code>/custom avatar approve [user]</code>: approves the user's avatar request. Requires: @ or above<br />` +
+					`<code>/custom avatar deny [user]</code>: denies the user's avatar request. Requires: @ or above<br />` +
+					`<code>/custom avatar on</code>: enables your own custom avatar.<br />` +
+					`<code>/custom avatar off</code>: disables your own custom avatar.<br />` +
+					`<code>/custom avatar blobbos</code>: enables the covetted Blobbos avatar.<br />`
+				);
+			},
 		},
 		title: {
 			set(target, room, user) {
@@ -344,14 +344,14 @@ export const commands: Chat.ChatCommands = {
 				saveCustomTitles();
 
 				this.sendReply('|raw| Your title was successfully unset. Log in again for it to disappear.');
-			}
-		},
-		'': 'titlehelp',
-		titlehelp() {
-			this.sendReplyBox(
-				`<code>/custom title set [title]</code>: sets your title to the desired text.<br />` +
-				`<code>/custom title unset</code>: removes your title.`
-			);
+			},
+			'': 'help',
+			help() {
+				this.sendReplyBox(
+					`<code>/custom title set [title]</code>: sets your title to the desired text.<br />` +
+					`<code>/custom title unset</code>: removes your title.`
+				);
+			},
 		},
 		flair: {
 			async set(target, room, user) {	
@@ -392,17 +392,18 @@ export const commands: Chat.ChatCommands = {
 				saveCssConfig();
 
 				this.sendReply('|raw| Your flair was successfully unset. It may take a while for it to dissapear.');
-			}
-		},
-		'': 'flairhelp',
-		flairhelp() {
-			this.sendReplyBox(
-				`<code>/custom flair set [pokemon], [mod], [heightOffset]</code>: sets your flair to the desired pokemon from the specified mod (pokemon or clover) with an optional height adjustment.<br />` +
-				`<code>/custom flair unset</code>: removes your flair.`
-			);
+			},
+			'': 'help',
+			help() {
+				this.sendReplyBox(
+					`<code>/custom flair set [pokemon], [mod], [heightOffset]</code>: sets your flair to the desired pokemon from the specified mod (pokemon or clover) with an optional height adjustment.<br />` +
+					`<code>/custom flair unset</code>: removes your flair.`
+				);
+			},
 		},
 	},
-	customhelp() {
+	'': 'help',
+	help() {
 		this.sendReplyBox(
 			`<code>/custom avatar</code>: commands related to custom avatars. Try <code>/help custom avatar</code> for details.<br />` +
 			`<code>/custom flair</code>: commands related to custom flairs. Try <code>/help custom flair</code> for details.<br />` +
