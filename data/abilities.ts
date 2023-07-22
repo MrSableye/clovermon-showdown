571d39596ee7c77410f5205533375a6dac80dc57
SDM0 <60699143+SDM0@users.noreply.github.com>
2023-07-22 03:18:48 +0200

    Fixed "Firewall"

--- data/abilities.ts
+++ data/abilities.ts
@@ -12644,7 +12644,7 @@ export const Abilities: {[abilityid: string]: AbilityData} = {
 				totalatk += target.getStat('atk', false, true);
 				totalspa += target.getStat('spa', false, true);
 			}
-			if (totalatk && totalspa >= totalspa) {
+			if (totalatk && totalatk >= totalspa) {
 				this.boost({def: 1});
 			} else if (totalspa) {
 				this.boost({spd: 1});
