a22fd1acd86664f7cafc6b8263bc6a5d171e0e7b
Mr. Sableye <mister.sableye@gmail.com>
2023-08-13 19:24:09 -0500

    [CAP] Fixes logic for Unnerve

--- data/mods/clovercap/scripts.ts
+++ data/mods/clovercap/scripts.ts
@@ -9,7 +9,7 @@ export const Scripts: ModdedBattleScriptsData = {
 				this.itemState.knockedOff || // Gen 3-4
 				(this.battle.gen >= 5 && !this.isActive) ||
 				(!this.getItem().ignoreKlutz && this.hasAbility(['klutz', 'originalsin'])) ||
-				(enemyUnnerving && this.hasItem(recoveryFoods)) ||
+				(enemyUnnerving && recoveryFoods.includes(this.item)) ||
 				this.volatiles['embargo'] || this.battle.field.pseudoWeather['magicroom']
 			);
 		}
