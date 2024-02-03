de9c9a1d0a53a23d1017135d4b3a5e82ae0eea14
Mr. Sableye <mister.sableye@gmail.com>
2024-02-03 14:19:04 -0600

    [Clover Cap] Buffs Gampster's Atk, SpA, and Spe by 5

---
+++ data/mods/clovertest/pokedex.ts
@@ -0,0 +1,13 @@
+export const Pokedex: {[k: string]: ModdedSpeciesData} = {
+	gampster: {
+		inherit: true,
+		baseStats: {
+			hp: 89,
+			atk: 90,
+			def: 51,
+			spa: 84,
+			spd: 54,
+			spe: 92,
+		},
+	},
+};
