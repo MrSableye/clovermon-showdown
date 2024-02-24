da4576558a3c70ec6e5bc3a0a9709494c2f5f7fe
Prolostique <117457604+Prolostique@users.noreply.github.com>
2024-02-24 22:17:37 +0100

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -56311,7 +56311,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		onTryMove(attacker, defender, move) {
 			if (attacker.removeVolatile(move.id)) {
 				return;
-			},
+			}
 			this.add('-prepare', attacker, move.name);
 			if (!this.runEvent('ChargeMove', attacker, defender, move)) {
 				return;
