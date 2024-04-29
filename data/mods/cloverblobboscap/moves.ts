e224689f8e2c283336cd7f56d863da5574910a06
C3ll <118361566+C3ll10u5@users.noreply.github.com>
2024-04-29 18:00:01 -0400

    potentially....makes dream eater epic in cab

--- data/mods/cloverblobboscap/moves.ts
+++ data/mods/cloverblobboscap/moves.ts
@@ -1823,6 +1823,31 @@ export const Moves: { [k: string]: ModdedMoveData } = {
 		accuracy: 100,
 		isNonstandard: null,
 	},
+	dreameater: {
+		num: 138,
+		accuracy: 100,
+		basePower: 65,
+		basePowerCallback(pokemon, target, move) {
+			return target.status === 'slp' || target.hasAbility('comatose') || target.hasAbility('boardpowerz'); {
+				this.debug('BP doubled from status condition');
+				return move.basePower * 2;
+			}
+			return move.basePower;
+		},
+		category: "Special",
+		name: "Dream Eater",
+		pp: 10,
+		priority: 0,
+		flags: {protect: 1, mirror: 1, heal: 1},
+		drain: [1, 2],
+		secondary: null,
+		target: "normal",
+		type: "Psychic",
+		zMove: {basePower: 160},
+		contestType: "Clever",
+		desc: "The initial damage of this move is altered only when the target is asleep. Then, this moves power is doubled. Only doubles in power for sleeping targets.",
+		shortDesc: "User gains 1/2 of targets HP inflicted. Doubles damage against sleeping targets.",
+	},
 	finishingtouch: {
 		inherit: true,
 		isNonstandard: null,
