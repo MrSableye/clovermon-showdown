275cb79ac6e388bda53a664dbab364481733c14a
C3ll <118361566+C3ll10u5@users.noreply.github.com>
2024-08-01 00:09:39 -0400

    item availability

--- data/mods/WIPMONS/items.ts
+++ data/mods/WIPMONS/items.ts
@@ -68,6 +68,186 @@ export const Items: {[k: string]: ModdedItemData} = {
 		inherit: true,
 		isNonstandard: null,
 	},
+	blunderpolicy: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	dracoplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	dreadplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	earthplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	ejectpack: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	fistplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	flameplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	icicleplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	insectplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	ironplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	meadowplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	mindplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	pixieplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	redcard: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	skyplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	splashplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	spookyplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	stoneplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	throatspray: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	toxicplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	utilityumbrella: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	zapplate: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	protectivepads: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	snowball: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	cellbattery: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	ringtarget: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	roomservice: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	grassyseed: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	electricseed: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	psychicseed: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	mistyseed: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	metronome: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	adrenalineorb: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	luminousmoss: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	absorbbulb: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	fullincense: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	oddincense: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	rockincense: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	roseincense: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	waveincense: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	rowapberry: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	loadeddice: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	jabocaberry: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	punchingglove: {
+		inherit: true,
+		isNonstandard: null,
+	},
+	abilityshield: {
+		inherit: true,
+		isNonstandard: null,
+	},
 	/* Disabled items */
 	absorbbulb: {
 		inherit: true,
