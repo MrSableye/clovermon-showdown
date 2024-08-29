a2660f9933975090c337814bcc4f4947f4ff3bf7
C3ll <118361566+C3ll10u5@users.noreply.github.com>
2024-08-29 19:32:19 -0400

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -1462,7 +1462,7 @@ export const Formats: FormatList = [
 			'Dynamax Clause',
 			'Sketch Post-Gen 7 Moves',
 		],
-		banlist: ['Baton Pass', 'Moody', 'Mating Press', 'Rage Fist', 'Revival Blessing', 'Shed Tail', 'Loaded Disk', 'Banana', 'Glomp :3', 'Sexite Y', 'Nimble Metal Body', 'Arena Trap', 'Shadow Tag', 'Fling + License to Sell Hotdogs', 'Mitosis Mash', 'Cell Construct', 'Power Herb + Geomancy', 'Last Respects + Blobbos-Paldea', 'Huge Power + Blobbos-Chad', 'Pure Power + Blobbos-Chad', 'Power Herb + Awaken', 'Baitite', 'Star Rod + Victory Dance', 'Blobbos-Zero', 'A Blobbos', 'Blobbos-Adventurer', 'Blobbos-Extradimensional', 'Blobbos-Nice', 'Blobbos-Lich', 'Blobbos-Gay', 'Infection', 'Blobbos-Arceus', 'Blobbos-Clover', 'Blobbos-Dark Matter', 'Blobbos-Forbidden', 'Blobbos-Horse', 'Blobbos-Primal', "Partner's Pendant + Super Snore"],
+		banlist: ['Baton Pass', 'Moody', 'Mating Press', 'Rage Fist', 'Revival Blessing', 'Shed Tail', 'Loaded Disk', 'Banana', 'Glomp :3', 'Sexite Y', 'Nimble Metal Body', 'Arena Trap', 'Shadow Tag', 'Fling + License to Sell Hotdogs', 'Mitosis Mash', 'Cell Construct', 'Power Herb + Geomancy', 'Last Respects + Blobbos-Paldea', 'Huge Power + Blobbos-Chad', 'Pure Power + Blobbos-Chad', 'Power Herb + Awaken', 'Baitite', 'Star Rod + Victory Dance', 'A Blobbos', 'Blobbos-Adventurer', 'Blobbos-Extradimensional', 'Blobbos-Nice', 'Blobbos-Lich', 'Blobbos-Gay', 'Infection', 'Blobbos-Arceus', 'Blobbos-Clover', 'Blobbos-Dark Matter', 'Blobbos-Zero', 'Blobbos-Forbidden', 'Blobbos-Horse', 'Blobbos-Primal', "Partner's Pendant + Super Snore"],
 		unbanlist: ['Blobbos-King'],
 	},
 	{
