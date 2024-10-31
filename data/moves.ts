cd4bbc768c65514c3c9cd2d0c3e039fe8ae417c8
DeeGee22 <dg22.clover@gmail.com>
2024-10-30 18:02:19 -0700

    forgor kino

--- data/moves.ts
+++ data/moves.ts
@@ -86111,7 +86111,10 @@ export const Moves: {[moveid: string]: MoveData} = {
 		pp: 5,
 		priority: 0,
 		flags: {contact: 1, protect: 1, mirror: 1, punch: 1},
-		secondary: null,
+		secondary: {
+			chance: 100,
+			status: 'brn',
+		},
 		target: "normal",
 		type: "Fire",
 		isNonstandard: "Future",
