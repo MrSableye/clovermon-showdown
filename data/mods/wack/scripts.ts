d0d4945c0f7923b38a50b5c681f7b980627eb1d6
DeeGee22 <dg22.clover@gmail.com>
2024-11-06 14:55:18 -0800

    Maybe changes Wack Crit Rate or Crashes server

--- data/mods/wack/scripts.ts
+++ data/mods/wack/scripts.ts
@@ -1,4 +1,13 @@
 export const Scripts: ModdedBattleScriptsData = {
 	gen: 9,
 	inherit: 'gen8',
+	actions: {
+		inherit: true,
+	modifyDamage(baseDamage, pokemon, target, move, suppressMessages = false) {
+		const isCrit = target.getMoveHitData(move).crit;
+			if (isCrit) {
+				baseDamage = this.battle.modify(baseDamage, move.critModifier || 2);
+			}
+		},
+	},
 };
