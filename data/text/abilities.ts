9e2c56e092c6b6b7f1f76b406c6764daa9396273
C3ll <118361566+C3ll10u5@users.noreply.github.com>
2024-12-11 14:00:48 -0500

    Update abilities.ts

--- data/text/abilities.ts
+++ data/text/abilities.ts
@@ -4025,10 +4025,6 @@ malediction: {
 		shortDesc: "Reduces Super Effective moves by 50%.",
 		desc: "Super effective damage taken is halved.",
 	},
-	mothsmajesty: {
-		name: "Moth's Majesty",
-		shortDesc: "Raises highest stat by 1 on switch-in.",
-	},
 	nuclearization: {
 		name: "Nuclearization",
 		shortDesc: "Normal type moves are Super Effective on Poison and Normal types.",
