25cc05c7162658e1973ae82eff1e0e21c39926a8
C3ll <118361566+C3ll10u5@users.noreply.github.com>
2024-12-24 16:58:39 -0500

    fixes

--- data/text/moves.ts
+++ data/text/moves.ts
@@ -7831,8 +7831,8 @@ export const MovesText: {[k: string]: MoveText} = {
 	},
 	evocation: {
 		name: "Evocation",
-		desc: "Lowers the user's Atk and Spatk by 1 stage.",
-		shortDesc: "Lowers the user's Atk and Sp. Atk by 1 stage. hysical if user's Atk > Sp. Atk.",
+		desc: "Lowers the user's Atk and Spatk by 1 stage. Physical if user's Atk > Sp. Atk.",
+		shortDesc: "Lowers the user's Atk and Sp. Atk by 1 stage. Physical if user's Atk > Sp. Atk.",
 	},
 	awaken: {
 		name: "Awaken",
