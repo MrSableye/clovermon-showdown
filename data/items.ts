1b5874cefec4d1c623b6e0f44d39cccc2347914a
C3ll <118361566+C3ll10u5@users.noreply.github.com>
2025-03-01 23:04:13 -0500

    theres no way right

--- data/items.ts
+++ data/items.ts
@@ -8575,11 +8575,11 @@ export const Items: {[itemid: string]: ItemData} = {
 			basePower: 90,
 		},
 		onAnyEffectiveness(typemod, target, type, move) {
-			const degradationUser = this.effectState.target;
+			const efficientpickUser = this.effectState.target;
 
-			if (degradationUser !== this.activePokemon) return; 
+			if (efficientpickUser !== 'Primiteve') return; 
 
-			if (pokemon.baseSpecies.name === 'Primiteve' && move.type === 'Rock' && ['Steel'].includes(type)) {
+			if (move.type === 'Rock' && ['Steel'].includes(type)) {
 				return 1;
 			}
 		},
