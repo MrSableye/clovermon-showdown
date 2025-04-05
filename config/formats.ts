6f62e16571b87c47fb3377a2131eb1838a69dfdd
C3ll <118361566+C3ll10u5@users.noreply.github.com>
2025-04-04 22:24:18 -0400

    Update formats.ts

--- config/formats.ts
+++ config/formats.ts
@@ -1260,7 +1260,7 @@ export const Formats: FormatList = [
 			'Dynamax Clause',
 			'Sketch Post-Gen 7 Moves',
 		],
-		banlist: ['Uber', 'Arena Trap', 'Moody', 'Power Construct', 'Shadow Tag', 'Baton Pass', 'Wonder Guard', 'Inversion'],
+		banlist: ['Uber', 'Arena Trap', 'Moody', 'Power Construct', 'Shadow Tag', 'Baton Pass', 'Wonder Guard', 'Inversion', 'Sand Veil'],
 	},
 	{
 		name: '[Gen 8 Clover CAP Only] NFE',
