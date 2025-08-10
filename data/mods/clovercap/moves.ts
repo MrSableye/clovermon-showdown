7373daf575d6fa46ac75f3f25fa965ab536a2c46
C3ll <118361566+C3ll10u5@users.noreply.github.com>
2025-08-10 17:37:26 -0400

    smart strike fix potentially

--- data/mods/clovercap/moves.ts
+++ data/mods/clovercap/moves.ts
@@ -566,9 +566,14 @@ export const Moves: { [k: string]: ModdedMoveData } = {
 	smartstrike: {
 		inherit: true,
 		isNonstandard: null,
-		boosts: {
-			accuracy: 1,
-		},
+		secondary: {
+					chance: 100,
+					self: {
+						boosts: {
+							accuracy: 1,
+						},
+					},
+				},
 	},
 	shellsidearm: {
 		inherit: true,
