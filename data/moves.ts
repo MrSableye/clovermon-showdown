dd9b6483a24fe0eaa3b3cf28db4ce66d36e0e569
DeeGee22 <dg22.clover@gmail.com>
2025-08-30 22:05:54 -0700

    Update moves.ts

--- data/moves.ts
+++ data/moves.ts
@@ -79950,6 +79950,7 @@ export const Moves: {[moveid: string]: MoveData} = {
 		priority: 0,
 		flags: {contact: 1, protect: 1, mirror: 1},
 		secondary: null,
+		recoil: [1, 2],
 		target: "normal",
 		type: "Cosmic",
 		isNonstandard: "Future",
