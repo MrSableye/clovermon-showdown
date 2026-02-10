6dfc36ece0391c26bc2a8a669734e29859631154
C3ll <118361566+C3ll10u5@users.noreply.github.com>
2026-02-09 23:59:03 -0500

    Update abilities.ts

--- data/mods/cloverblobboscap/text/abilities.ts
+++ data/mods/cloverblobboscap/text/abilities.ts
@@ -4,11 +4,6 @@ export const AbilitiesText: {[k: string]: ModdedAbilityText} = {
 		desc: "If an active ally has this Ability or the Minus Ability, this Pokemon's Special Attack is multiplied by 1.5. After an Electric-type move hits, has a 25% chance to boost a random stat (not acc/eva).",
 		shortDesc: "If an active ally has this Ability or the Minus Ability, this Pokemon's Sp. Atk is 1.5x. After an Electric-type move hits, has a 25% chance to boost a random stat (not acc/eva).",
 	},
-	regenerator: {
-		name: "Regenerator",
-		desc: "When the holder of this ability is switched out, 20% of their current max hp is recovered.",
-		shortDesc: "This Pokemon restores 1/5 of its maximum HP when its switched out.",
-	},
 	minus: {
 		name: "Minus",
 		desc: "If an active ally has this Ability or the Plus Ability, this Pokemon's Special Attack is multiplied by 1.5. Prevents this Pokemon's stats from being lowered.",
