8cb70fc1b6fbcba1f60ab01a0fbee99a58151fe3
C3ll <118361566+C3ll10u5@users.noreply.github.com>
2026-03-05 20:11:14 -0500

    g

--- data/pokedex.ts
+++ data/pokedex.ts
@@ -37324,7 +37324,7 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		evoItem: "Dawn Stone",
 		eggGroups: ["Human-Like"],
 		gen: 8,
-		creator: "Bugs",
+		creator: "Bugs/Zac",
 	 },
 	 vergilion: {
 		num: -42669,
@@ -37341,7 +37341,7 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		evoItem: "Dusk Stone",
 		eggGroups: ["Human-Like"],
 		gen: 8,
-		creator: "Bugs",
+		creator: "Bugs/Zac",
 	 },
 	 leopawn: {
 		num: -8624,
@@ -50869,7 +50869,7 @@ export const Pokedex: {[speciesid: string]: SpeciesData} = {
 		evos: ["Virtuita"],
 		eggGroups: ["Grass", "Fairy"],
 		gen: 8,
-		creator: "Con",
+		creator: "Con/Rexcanyon792",
 	},
 	peashroom: {
 		num: 42168,
@@ -53364,7 +53364,7 @@ bewilderbeast: {
 		gen: 8,
 		creator: "The Fiend",
 	},
-	swinggrub: {
+	swingrub: {
 		num: -8636,
 		name: "Swingrub",
 		types: ["Bug", "Poison"],
