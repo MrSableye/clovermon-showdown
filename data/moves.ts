c38dbf9fa7af5c0ed6df60566b32bc64bd7a4da0
Greednut <115999607+Greednut@users.noreply.github.com>
2026-08-18 13:45:26 -0300

    sigh

--- data/moves.ts
+++ data/moves.ts
@@ -77269,6 +77269,30 @@ export const Moves: {[moveid: string]: MoveData} = {
         type: "Ice",
         isNonstandard: "Future",
     },
+	auxiliarypower: {
+		accuracy: 100,
+		basePower: 60,
+		category: "Special",
+		name: "Auxilary Power",
+		pp: 5,
+		priority: 0,
+		flags: {protect: 1, mirror: 1},
+		secondary: {
+			chance: 10,
+			self: {
+				boosts: {
+					atk: 1,
+					def: 1,
+					spa: 1,
+					spd: 1,
+					spe: 1,
+				},
+			},
+		},
+		target: "normal",
+		type: "Electric",
+		contestType: "Tough",
+	},
     wakingchant: { 
         num: 668748,
         accuracy: 100,
